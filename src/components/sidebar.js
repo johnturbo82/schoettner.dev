5d6bc27c02265950524854710ac27ff9311ea4bf
Oliver Schöttner <johnturbo82@gmail.com>
2021-04-05 19:06:28 +0200

    logo added

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { scaleDown as Menu } from 'react-burger-menu'
+import logo from "../images/jt.png"
 
 const SideBar = (props) => {
   return (
     <Menu pageWrapId={props.pageWrapId} outerContainerId={props.outerContainerId} >
+        <img src={logo} alt="JT1 Logo" />
         <a id="home" className={(props.path === "/")? "menu-item active" : "menu-item"} href="/">Home</a>
         <a id="blog" className={(props.path === "/blog" || props.path.includes("/blog"))? "menu-item active" : "menu-item"} href="/blog">Blog</a>
         <a id="about" className={(props.path === "/about")? "menu-item active" : "menu-item"} href="/about">About</a>
