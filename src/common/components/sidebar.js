5acceea68bff4fcd3bb9a528121d65815146461c
Oliver Schöttner <johnturbo82@gmail.com>
2021-04-08 16:37:06 +0200

    Sidebar Links

--- src/common/components/sidebar.js
+++ src/common/components/sidebar.js
@@ -15,20 +15,20 @@ const SideBar = (props) => {
                     <a id="home" className={(props.path === "/") ? "menu-item active" : "menu-item"} href="/">Home</a>
                     <a id="blog" className={(props.path === "/blog" || props.path.includes("/blog")) ? "menu-item active" : "menu-item"} href="/blog">Blog</a>
                     <a id="about" className={(props.path === "/about") ? "menu-item active" : "menu-item"} href="/about">About</a>
-                    <a id="contact" className={(props.path === "/contact") ? "menu-item active" : "menu-item"} href="/Contact">Home</a>
+                    <a id="contact" className={(props.path === "/contact") ? "menu-item active" : "menu-item"} href="/contact">Contact</a>
                 </div>
                 <div className="sidebar_mood" >
                     <h3>Mood Selector</h3>
                     <a href="https://schoettner.rocks" className="mood" style={{ backgroundImage: `url(${schoettner})` }} alt="schoettner.rocks">
                         <div>Software Developer</div>
                     </a>
-                    <a href="https://schoettner.rocks" className="mood" style={{ backgroundImage: `url(${johnturbo})` }} alt="johnturbo.com">
+                    <a href="https://johnturbo.com" className="mood" style={{ backgroundImage: `url(${johnturbo})` }} alt="johnturbo.com">
                         <div>Stars & Stripes</div>
                     </a>
-                    <a href="https://schoettner.rocks" className="mood" style={{ backgroundImage: `url(${jt1})` }} alt="jt1.rocks">
+                    <a href="https://jt1.rocks" className="mood" style={{ backgroundImage: `url(${jt1})` }} alt="jt1.rocks">
                         <div>Rock n' Roll</div>
                     </a>
-                    <a href="https://schoettner.rocks" className="mood" style={{ backgroundImage: `url(${code})` }} alt="schoettner.dev">
+                    <a href="https://schoettner.dev" className="mood" style={{ backgroundImage: `url(${code})` }} alt="schoettner.dev">
                         <div>Nerdy</div>
                     </a>
                 </div>
