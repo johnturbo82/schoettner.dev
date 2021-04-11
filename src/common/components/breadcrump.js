daee389af434177be4f26650861980e5e806b896
Oliver Schoettner <johnturbo82@gmail.com>
2021-04-11 09:36:14 +0200

    Breadcrump padding

--- src/common/components/breadcrump.js
+++ src/common/components/breadcrump.js
@@ -3,14 +3,16 @@ import { Link } from 'gatsby'
 
 const BreadCrump = (props) => {
     return (
-        <div className="breadcrump">
-            <Link to="/">Startseite</Link>
-            {props.parent && (
-                <span>&nbsp;&raquo; <Link to={"/" + props.parent.toLowerCase()}>{props.parent}</Link></span>
-            )}
-            {props.sitetitle !== "Startseite" && (
-                <span>&nbsp;&raquo; {props.sitetitle}</span>
-            )}
+        <div className="breadcrump-container">
+            <div className="breadcrump">
+                <Link to="/">Startseite</Link>
+                {props.parent && (
+                    <span>&nbsp;&raquo; <Link to={"/" + props.parent.toLowerCase()}>{props.parent}</Link></span>
+                )}
+                {props.sitetitle !== "Startseite" && (
+                    <span>&nbsp;&raquo; {props.sitetitle}</span>
+                )}
+            </div>
         </div>
     )
 }
