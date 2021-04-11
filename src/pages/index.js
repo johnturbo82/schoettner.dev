dff61e06d88ceddd7c4070b8217478f0e6a05b40
Oliver Schoettner <johnturbo82@gmail.com>
2021-04-11 16:35:02 +0200

    page template removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../common/components/layout'
 
@@ -7,6 +8,7 @@ const IndexPage = () => {
         <Layout sitetitle="Startseite" path="/">
             <h1>Hello World</h1>
             <p>Test</p>
+            <Link to="/kontakt">Kontakt</Link>
         </Layout>
     )
 }
