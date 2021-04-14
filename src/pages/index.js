19c72d2418eade56d9c24c02a4eaec0d8a490d12
Oliver Schoettner <johnturbo82@gmail.com>
2021-04-14 18:06:37 +0200

    minor

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
                 <p>Ich arbeite aktuell bei <a target="_blank" rel="noreferrer" href="https://cariad.technology/">CARIAD SE</a>, dem Softwarehaus des Volkswagen Konzerns. Dort mache ich vor allem <em>Python</em>, aber auch etwas Frontend <em>React</em>.</p>
                 <p>Bei meinem letzten Arbeitgeber BFFT Gesellschaft für Fahrzeugtechnik mbH, jetzt EDAG Engineering GmbH, habe ich noch viel <em>PHP </em> und <em>C#</em> gemacht. Das waren noch Zeiten...</p>
                 <p>Meine Freizeit verbringe ich im Sommer gerne auf einem Motorrad oder bei Konzerten.</p>
-                <p>Sollte ich zu hause auf der Couch sein, läuft Eishockey oder Fussball <em>(oder beides)</em> oder eine Netflixserie.</p>
+                <p>Sollte ich zu Hause auf der Couch sein, läuft Eishockey oder Fussball <em>(oder beides)</em> oder eine Netflixserie.</p>
                 <p>Diese Seite gibt's nur, weil viele Neugierige die Domain meiner Email-Adresse in den Browser tippen und dann enttäuscht sind, dass es keinen Inhalt gibt. Solltest Du auch auf diesem Wege zu mir gekommen sein: <em>Hallo!</em></p>
             </div>
             <Passion />
