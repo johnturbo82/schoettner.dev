18f46dccff43bef45dede6c81fcfba0d150b2309
Oliver Schöttner <johnturbo82@gmail.com>
2021-04-15 13:23:03 +0200

    Menu load with dict

--- src/common/components/sidebar.js
+++ src/common/components/sidebar.js
@@ -4,6 +4,13 @@ import Moods from './moods'
 import logo from "../../images/jt.svg"
 
 const SideBar = (props) => {
+    const sites = [
+        ["/", "Startseite"],
+        ["/blog", "Blog"],
+        ["/ueber-mich", "Über mich"],
+        ["/kontakt", "Kontakt"]
+    ];
+
     return (
         <>
             <div className="overlay" aria-label="Close Menu" role="button" onClick={props.handleClose} onKeyDown={props.handleEscKey} tabIndex={-2}></div>
@@ -11,10 +18,7 @@ const SideBar = (props) => {
                 <span className="close" title="Close Menu" role="button" onClick={props.handleClose} onKeyDown={props.handleEscKey} tabIndex={-1}>x</span>
                 <div className="sidebar_nav" >
                     <img className="logo" src={logo} alt="JT1 Logo" />
-                    <a id="home" className={(props.path === "/") ? "menu-item active" : "menu-item"} href="/">Startseite</a>
-                    <a id="blog" className={(props.path === "/blog" || props.path.includes("/blog")) ? "menu-item active" : "menu-item"} href="/blog">Blog</a>
-                    <a id="about" className={(props.path === "/about") ? "menu-item active" : "menu-item"} href="/ueber-mich">Über mich</a>
-                    <a id="contact" className={(props.path === "/contact") ? "menu-item active" : "menu-item"} href="/kontakt">Kontakt</a>
+                    {sites.map((val, key) => (<a key={key} id={"menu_" + key} className={(props.path === val[0] || (props.path.includes("/blog") && val[0] == "/blog")) ? "menu-item active" : "menu-item"} href={val[0]}>{val[1]}</a>))}
                 </div>
                 <Moods />
             </div>
