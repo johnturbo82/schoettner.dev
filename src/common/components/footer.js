c1364c70a674c68ec6701201175b1b34df39cab3
Oliver Schoettner <johnturbo82@gmail.com>
2021-04-16 15:56:27 +0200

    bugfix footer overlay

--- src/common/components/footer.js
+++ src/common/components/footer.js
@@ -27,26 +27,28 @@ const Footer = () => {
 
     return (
         <footer className="footer">
-            <h2>Impressum</h2>
-            <p>{data.site.siteMetadata.author}<br />{data.site.siteMetadata.street}<br />{data.site.siteMetadata.postal_code} {data.site.siteMetadata.city}</p>
-            <p><a href={data.site.siteMetadata.mailto}>{data.site.siteMetadata.email}</a></p>
-            <p><a href={data.site.siteMetadata.mobileto}>{data.site.siteMetadata.mobile}</a></p>
-            <div className="social_icons">
-                <a href="https://github.com/johnturbo82/" title="Github" className="github">
-                    <img src={Github} alt="Github" title="Github" />
-                </a>
-                <a href="https://www.linkedin.com/in/oliver-schoettner/" title="LinkedIn" className="linkedin">
-                    <img src={LinkedIn} alt="LinkedIn" title="LinkedIn" />
-                </a>
-                <a href="https://twitter.com/johnturbo82" title="Twitter" className="twitter">
-                    <img src={Twitter} alt="Twitter" title="Twitter" />
-                </a>
-                <a href="https://www.instagram.com/johnturbo82/" title="Instagram" className="instagram">
-                    <img src={Instagram} alt="Instagram" title="Instagram" />
-                </a>
-                <a href="https://www.facebook.com/johnturbo82/" title="Facebook" className="facebook">
-                    <img src={Facebook} alt="Facebook" title="Facebook" />
-                </a>
+            <div className="stage">
+                <h2>Impressum</h2>
+                <p>{data.site.siteMetadata.author}<br />{data.site.siteMetadata.street}<br />{data.site.siteMetadata.postal_code} {data.site.siteMetadata.city}</p>
+                <p><a href={data.site.siteMetadata.mailto}>{data.site.siteMetadata.email}</a></p>
+                <p><a href={data.site.siteMetadata.mobileto}>{data.site.siteMetadata.mobile}</a></p>
+                <div className="social_icons">
+                    <a href="https://github.com/johnturbo82/" title="Github" className="github">
+                        <img src={Github} alt="Github" title="Github" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/oliver-schoettner/" title="LinkedIn" className="linkedin">
+                        <img src={LinkedIn} alt="LinkedIn" title="LinkedIn" />
+                    </a>
+                    <a href="https://twitter.com/johnturbo82" title="Twitter" className="twitter">
+                        <img src={Twitter} alt="Twitter" title="Twitter" />
+                    </a>
+                    <a href="https://www.instagram.com/johnturbo82/" title="Instagram" className="instagram">
+                        <img src={Instagram} alt="Instagram" title="Instagram" />
+                    </a>
+                    <a href="https://www.facebook.com/johnturbo82/" title="Facebook" className="facebook">
+                        <img src={Facebook} alt="Facebook" title="Facebook" />
+                    </a>
+                </div>
             </div>
         </footer>
     )
