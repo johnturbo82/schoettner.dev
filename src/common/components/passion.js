35ff4db1b354ae74a1f1589c9846bc57c9a1e721
Oliver Schoettner <johnturbo82@gmail.com>
2021-04-17 20:22:11 +0200

    random passion pics

--- src/common/components/passion.js
+++ src/common/components/passion.js
@@ -27,21 +27,48 @@ const Passion = () => {
                         }
                     }
                 }
+                Metallica: file(relativePath: { eq: "passion/Metallica.jpg" }) {
+                    childImageSharp {
+                        fluid(quality: 90, maxWidth: 400) {
+                            ...GatsbyImageSharpFluid_withWebp
+                        }
+                    }
+                }
+                Development: file(relativePath: { eq: "passion/code.png" }) {
+                    childImageSharp {
+                        fluid(quality: 90, maxWidth: 400) {
+                            ...GatsbyImageSharpFluid_withWebp
+                        }
+                    }
+                }
             }
 
         `
     )
+    const mood_pics = [
+        ["ERC Ingolstadt", data.ERCIngolstadt.childImageSharp.fluid],
+        ["TSV 1860 München", data.TSV1860.childImageSharp.fluid],
+        ["Motorrad", data.Motorrad.childImageSharp.fluid],
+        ["Metallica", data.Metallica.childImageSharp.fluid],
+        ["Development", data.Development.childImageSharp.fluid],
+    ]
+    const randomGenerator = () => {
+        var array = Array.from({length:mood_pics.length},(v,k)=>k);
+        var shuffled = array.sort(function(){return 0.5 - Math.random()});
+        return shuffled.slice(0,3);
+    }
+    const randomlist = randomGenerator()
     return (
         <div className="passion" >
             <h3>Passion</h3>
-            <BackgroundImage className="passion_icon" fluid={data.Motorrad.childImageSharp.fluid}>
-                <div>Motorrad</div>
+            <BackgroundImage className="passion_icon" fluid={mood_pics[randomlist[0]][1]}>
+                <div>{mood_pics[randomlist[0]][0]}</div>
             </BackgroundImage>
-            <BackgroundImage className="passion_icon" fluid={data.TSV1860.childImageSharp.fluid}>
-                <div>TSV 1860 München</div>
+            <BackgroundImage className="passion_icon" fluid={mood_pics[randomlist[1]][1]}>
+                <div>{mood_pics[randomlist[1]][0]}</div>
             </BackgroundImage>
-            <BackgroundImage className="passion_icon" fluid={data.ERCIngolstadt.childImageSharp.fluid}>
-                <div>ERC Ingolstadt</div>
+            <BackgroundImage className="passion_icon" fluid={mood_pics[randomlist[2]][1]}>
+                <div>{mood_pics[randomlist[2]][0]}</div>
             </BackgroundImage>
         </div>
     );
