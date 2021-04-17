54c9b1458a01d4597b6123735ac1a2dd3e146549
Oliver Schoettner <johnturbo82@gmail.com>
2021-04-17 08:36:44 +0200

    script adapted

--- bulk_cp_pages_and_checkin.sh
+++ bulk_cp_pages_and_checkin.sh
@@ -4,7 +4,7 @@ git add ./src/templates ./src/pages
 git commit -m "$1"
 git push
 
-for folder in johnturbo.com schoettner.rocks
+for folder in johnturbo.com schoettner.rocks pure.schoettner.dev
 do
     FILE="../$folder"
     if [ -d "$FILE" ]; then
--- bulk_pull.sh
+++ bulk_pull.sh
@@ -2,7 +2,7 @@
 
 git pull
 
-for folder in johnturbo.com schoettner.rocks
+for folder in johnturbo.com schoettner.rocks pure.schoettner.dev
 do
     FILE="../$folder"
     if [ -d "$FILE" ]; then
