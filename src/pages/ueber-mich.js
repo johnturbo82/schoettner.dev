674f3a11d14da5a292e4d68eea7bd1a2811c1061
Oliver Schoettner <johnturbo82@gmail.com>
2021-04-20 16:52:27 +0200

    content

--- src/pages/ueber-mich.js
+++ src/pages/ueber-mich.js
@@ -10,9 +10,9 @@ const AboutPage = () => {
             <p>Wie Dir vielleicht aufgefallen ist, gibt es für die Website drei Themes, die alle unter unterschiedlichen Domains erreichbar sind. Der Grund dafür ich einfach: Ich konnte mich nicht entscheiden, welches Thema ich meiner Website zugrunde legen will.</p>
             <p>So ist auch die "Über mich"-Seite in drei Teilbereiche unterteilt:</p>
             <ul>
-                <li><a onClick={() => scrollTo('#entwicklung')}>Entwicklung</a></li>
-                <li><a onClick={() => scrollTo('#motorradfahren')}>Motorradfahren</a></li>
-                <li><a onClick={() => scrollTo('#musik')}>Musik und Konzerte</a></li>
+                <li><button onClick={() => scrollTo('#entwicklung')}>Entwicklung</button></li>
+                <li><button onClick={() => scrollTo('#motorradfahren')}>Motorradfahren</button></li>
+                <li><button onClick={() => scrollTo('#musik')}>Musik und Konzerte</button></li>
             </ul>
             <p>Über das Menü kannst Du unter "Themes" das passende Template auswählen, wenn Du Lust hast. Dann gibt es noch <a href="https://pure.schoettner.dev">pure.schoettner.dev</a>, einfach nur deshalb um auf grafische Gestaltung zu verzichten und zu testen, wie schnell dieses <em>Framework</em> wirklich sein kann.</p>
             <h2 id="entwicklung">Entwicklung</h2>
