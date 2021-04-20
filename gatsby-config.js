57792ef5e38d2fcd846f38376eba20fd4cde1712
Oliver Schoettner <johnturbo82@gmail.com>
2021-04-20 16:27:23 +0200

    smooth scroll added

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         "gatsby-plugin-image",
         "gatsby-plugin-react-helmet",
         "gatsby-plugin-sitemap",
+        "gatsby-plugin-smoothscroll",
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
