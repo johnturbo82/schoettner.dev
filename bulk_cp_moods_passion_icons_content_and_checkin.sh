42a7cedce7af13d1df2b745117cd5a43435c2b0e
Oliver Schöttner <johnturbo82@gmail.com>
2021-04-20 22:13:32 +0200

    scripts

--- bulk_cp_moods_passion_icons_content_and_checkin.sh
+++ bulk_cp_moods_passion_icons_content_and_checkin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git add ./src/images/moods ./src/images/passion ./src/images/social_icons
+git add ./src/images/moods ./src/images/passion ./src/images/social_icons ./src/images/content
 git commit -m "$1"
 git push
 
@@ -11,13 +11,15 @@ do
         rm -rf "$FILE/src/images/moods"
         rm -rf "$FILE/src/images/passion"
         rm -rf "$FILE/src/images/social_icons"
+        rm -rf "$FILE/src/images/content"
         cp -r "./src/images/moods" "$FILE/src/images"
         cp -r "./src/images/passion" "$FILE/src/images"
         cp -r "./src/images/social_icons" "$FILE/src/images"
+        cp -r "./src/images/content" "$FILE/src/images"
 
 
         cd $FILE
-        git add ./src/images/moods ./src/images/passion ./src/images/social_icons
+        git add ./src/images/moods ./src/images/passion ./src/images/social_icons ./src/images/content
         git commit -m "$1"
         git push
     fi
